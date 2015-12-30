3142d77c5c3392f943a74a296dd15c82befacd8f
Anjan Jagirdar <anjan.jagirdar@gmail.com>
2015-12-31 01:45:05 +0530

    Added aboout page and slug

---
+++ app/models/page.rb
@@ -0,0 +1,13 @@
+class Page < ActiveRecord::Base
+	# attr_accessible :body, :title, :slug
+
+def self.find(slug)
+    find_by_slug(slug)
+end
+
+
+def to_param
+	slug
+end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151224111151) do
+ActiveRecord::Schema.define(version: 20151230110505) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 20151224111151) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "slug"
+  end
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
